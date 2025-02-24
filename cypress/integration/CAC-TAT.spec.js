c4b0694c6ecbd2f87441cffcbc2240b46f7f569e
andrezambroni <andrezambroni@gmail.com>
2025-02-24 14:49:41 -0300

    aula 4 finalizada

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -97,14 +97,23 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get("#product").select("YouTube").should("have.value", "youtube")
   })
 
-   it("seleciona um produto (Mentoria) por seu valor (value)", function () {
-     cy.get("#product").select("mentoria").should("have.value", "mentoria")
-   })
+  it("seleciona um produto (Mentoria) por seu valor (value)", function () {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria")
+  })
 
-   it.only("seleciona um produto (Blog) por seu índice", function () {
-     cy.get("#product").select(1).should("have.value", "blog")
-   })
+  it("seleciona um produto (Blog) por seu índice", function () {
+    cy.get("#product").select(1).should("have.value", "blog")
+  })
 
-   
+  it("marca o tipo de atendimento 'Feedback'", function () {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should("have.value", "feedback")
+  })
 
+  it.only("marca cada tipo de atendimento", function () {
+    cy.get('input[type="radio"][value="ajuda"]').check().should("be.checked")
+    cy.get('input[type="radio"][value="elogio"]').check().should("be.checked")
+    cy.get('input[type="radio"][value="feedback"]').check().should("be.checked")
+  })
 })
